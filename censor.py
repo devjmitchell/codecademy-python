11cb4f24101e6df27f9c1eee7ea8c6bf189a94fa
semperphoenix <semperphoenixaz@gmail.com>
2017-01-12 10:41:43 -0700

    created censor.py

---
+++ censor.py
@@ -0,0 +1,13 @@
+def censor(text,word):
+    list1 = text.split()
+    list2 = []
+    for x in list1:
+        if x == word:
+            list2.append(len(x)*"*")
+        else:
+            list2.append(x)
+    new_phrase = " ".join(list2)
+    print new_phrase # to see what is being returned
+    return new_phrase
+
+censor("This hack is a hack test of hacks", "hack")
