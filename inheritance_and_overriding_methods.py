9a6f353a10d5a920bcc40d0416178452afa3e837
semperphoenix <semperphoenixaz@gmail.com>
2017-01-16 19:39:04 -0700

    created a few class files

---
+++ inheritance_and_overriding_methods.py
@@ -0,0 +1,26 @@
+# inheritance and overriding methods
+
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def display_car(self):
+        return "This is a %s %s with %s MPG." % (self.color, self.model, str(self.mpg))
+    def drive_car(self):
+        self.condition = "used"
+
+class ElectricCar(Car):
+    def __init__(self, model, color, mpg, battery_type):
+        self.battery_type = battery_type
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+    def drive_car(self):
+        self.condition = "like new"
+
+my_car = ElectricCar("Tesla", "black", "unlimited", "molten salt")
+print my_car.condition
+my_car.drive_car()
+print my_car.condition
